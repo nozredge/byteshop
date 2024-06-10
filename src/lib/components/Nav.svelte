a293f25dc12a06068cd8f3b27607f888b06c9d74
nozredge <nozredge@proton.me>
2024-06-10 18:27:12 +0000

    Add navbar

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,61 @@
+<nav>
+    <div class="container">
+        <div class="logo">
+
+        </div>
+
+        <div class="links-container">
+            <a href="#">Home</a>
+            <a href="#">Páginas</a>
+            <a href="#">Blog</a>
+            <a href="#">Contacto</a>
+        </div>
+
+        <div class="search-cart-container">
+            
+        </div>
+    </div>
+</nav>
+
+<style>
+    
+    /* Resetar CSS **/
+    :global(body) {     
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+    }
+
+    .links-container {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        background-color: #f4f4f8;
+        height: 15vh;
+        width: 60vw;
+    }
+
+    a {
+        text-decoration: none;
+        font-size: 1.2em;
+        color: #1e1e24;
+    }
+
+    a:hover{
+        color: #7a7877;
+        /*font-size: 1.3em;*/
+        text-decoration-thickness: 0.1em;
+        text-decoration-style: solid;
+        text-decoration-line: underline;
+    }
+
+    * {
+        font-family: "M PLUS Rounded 1c", sans-serif;
+    }
+
+    
+
+</style>
