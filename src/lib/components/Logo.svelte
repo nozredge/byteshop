d3ee8a1851ef31b6372243ab78cf23384bb094ca
nozredge <nozredge@proton.me>
2024-06-11 03:48:02 -0400

    Add Logo component

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,14 @@
+<script>
+    import logo from '$lib/assets/img/bytechile_logo_1/bytechile_logo_trans.png'
+</script>
+
+<div>
+    <img class="bytechile-logo" src="{logo}" alt="logo">
+</div>
+
+<style>
+    .bytechile-logo {
+        background-image: url('$');
+        width: 10vw;
+    }
+</style>
