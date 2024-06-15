4cb707114c8994522db879b000045adbb6127df7
nozredge <nozredge@proton.me>
2024-06-15 00:22:33 -0400

    Change Products title font

---
+++ src/lib/components/ProductsTitle.svelte
@@ -0,0 +1,22 @@
+<script>
+    import "@fontsource/montserrat";
+</script>
+
+<div>
+    <h3>Nuestros productos más vendidos</h3>
+</div>
+
+<style>
+
+    div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    div > h3 {
+        font-size: 2.3em;
+        color: #343a40;
+        font-family: "Montserrat";
+    }
+</style>
