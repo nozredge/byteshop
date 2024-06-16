e5a97eb4b1b0c2cae4276a6b4c1702d63b2d6af6
nozredge <nozredge@proton.me>
2024-06-16 10:20:39 -0400

    Change Agregar al carrito link to a nicer button

--- src/routes/ProductDetails.svelte
+++ src/routes/ProductDetails.svelte
@@ -1,14 +1,24 @@
-<script>
+<script lang="ts">
     import "@fontsource/montserrat";
 </script>
 
 <div class="details">
     <p>Producto</p>
-    <p>+ Agregar al carrito</p>
+    <button>+ Agregar al carrito</button>
     <p>$23.990</p>
 </div>
 
 <style>
+
+    button {
+        all:unset; 
+        border: 0.2rem solid #ccd1d1;
+        border-radius: 1.2rem;
+        cursor: pointer;
+        color: #555;
+        padding: 0.4rem;
+        text-align: center;
+    }
 
     div {
         display: grid;
@@ -25,16 +35,5 @@
 
     div :nth-child(3) {
         font-size: 1.4rem;
-    }
-
-    div :nth-child(2) {
-        color:#e84855;
-        font-size: 1.1rem;
-    }
-
-    div :nth-child(2):hover {
-        cursor: pointer;
-        color:#e84815;
-
     }
 </style>
