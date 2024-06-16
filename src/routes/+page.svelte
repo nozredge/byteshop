6340c515dd49a7fb7bc6293004da86ec3772ed0c
nozredge <nozredge@proton.me>
2024-06-15 22:49:14 -0400

    Add footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import Nav from "$lib/components/Nav.svelte"
     import Body from "$lib/components/Body.svelte"
+    import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <Nav />
 <Body />
+<Footer />
